c78d675166b24d6fa488ce1e84dd630a73b9f409
Kaung Pyae Min Thein <kaungpyaemt.kg@gmail.com>
2026-03-28 20:26:09 +0700

    server:set up web view

--- MCU_Server/config/urls.py
+++ MCU_Server/config/urls.py
@@ -1,24 +1,22 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from detection.views import process_waste
+from django.conf import settings
+from django.conf.urls.static import static
+from detection import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/waste/', process_waste, name='process_waste')
+    
+    # The API for the ESP32
+    path('api/waste/', views.process_waste, name='process_waste'),
+    
+    # The Frontend Dashboard
+    path('', views.dashboard, name='dashboard'),
+    
+    # The API for the Dashboard to get text updates
+    path('api/status/', views.get_latest_status, name='get_status'),
 ]
+
+# This line allows Django to serve the saved images to your browser
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
