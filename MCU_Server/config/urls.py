5c0a9dc4ebebb28bd48d0451c2db16570ee2dde9
Worapob Wannatang <woraphob.dmi@gmail.com>
2026-03-29 19:46:12 +0700

    Update urls.py

--- MCU_Server/config/urls.py
+++ MCU_Server/config/urls.py
@@ -1,6 +1,15 @@
+from django.contrib import admin
 from django.urls import path
-from detection import views
+from detection.views import dashboard, upload_image, get_latest_image
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('admin/', admin.site.urls),
+    path('', dashboard, name='dashboard'),
+    path('upload', upload_image, name='upload'), # Access way for receiving images from the board.
+    path('api/latest-image', get_latest_image, name='latest_image'), # The API allows the webpage to retrieve images.
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
