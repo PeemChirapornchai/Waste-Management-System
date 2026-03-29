cf5d78d72cdc6452394bc9d71d2ab338b18dc5f0
Worapob Wannatang <woraphob.dmi@gmail.com>
2026-03-29 19:46:23 +0700

    Update settings.py

--- MCU_Server/config/settings.py
+++ MCU_Server/config/settings.py
@@ -27,19 +27,28 @@ DEBUG = os.environ.get("DEBUG", "1") == "1"
 
 # 2. Wire ALLOWED_HOSTS to the environment variable.
 allowed_hosts_env = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,192.168.137.1")
-ALLOWED_HOSTS = allowed_hosts_env.split(",")
-
+# ALLOWED_HOSTS = allowed_hosts_env.split(",")
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',           # 
+    'django.contrib.auth',            # 
+    'django.contrib.contenttypes',    # 
+    'django.contrib.sessions',        # 
+    'django.contrib.messages',        # 
     'django.contrib.staticfiles',
     'detection',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware', # Auth
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -54,6 +63,8 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',      #
+                'django.contrib.messages.context_processors.messages', #
             ],
         },
     },
@@ -93,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
+# https://docs.djangoproject.com/en/4.2/topics/i18n/ 
 
 LANGUAGE_CODE = 'en-us'
 
@@ -108,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles" #
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
